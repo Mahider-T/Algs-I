379fa7cbe1a7939218e4631db86ba9210b154a94
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-10-06 10:23:52 +0300

    Complete week1

--- week1/HelloGoodbye/HelloGoodbye.java
+++ week1/HelloGoodbye/HelloGoodbye.java
@@ -2,10 +2,11 @@ package week1.HelloGoodbye;
 
 public class HelloGoodbye {
     public static void main(String[] args) {
-        if (args.length != 3) {
+        if (args.length != 2) {
             System.out.println("Only two command line arguments are allowed.");
         }else {
-            System.out.println(args[0]);
+            System.out.println("Hello " + args[0] + " and "+args[1]);
+            System.out.println("Goodbye " + args[1] + " and " + args[0]);
         }
 
     }
---
+++ week1/RandomWord/RandomWord.java
@@ -0,0 +1,25 @@
+package week1.RandomWord;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+
+        StdOut.println("Enter the string");
+        double i = 1;
+        String winner = "";
+
+        while(!StdIn.isEmpty()) {
+            String theString = StdIn.readString();
+            if (StdRandom.bernoulli(1/i)){
+                winner = theString;
+            }
+            i++;
+        }
+        StdOut.println(winner);
+    }
+
+    }
+

feb8a1dbf69d2b7a92d9af5faf47337ad84f5c64
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-10-06 11:21:50 +0300

    Make pretty

--- HelloGoodbye.java
+++ HelloGoodbye.java
@@ -1,9 +1,9 @@
-public class HelloGoodbye {
+class HelloGoodbye {
     public static void main(String[] args) {
         if (args.length != 2) {
             System.out.println("Only two command line arguments are allowed.");
-        }else {
-            System.out.println("Hello " + args[0] + " and "+args[1]);
+        } else {
+            System.out.println("Hello " + args[0] + " and " + args[1]);
             System.out.println("Goodbye " + args[1] + " and " + args[0]);
         }
 
--- RandomWord.java
+++ RandomWord.java
@@ -2,22 +2,21 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
-public class RandomWord {
+class RandomWord {
     public static void main(String[] args) {
 
         StdOut.println("Enter the string");
         double i = 1;
         String winner = "";
 
-        while(!StdIn.isEmpty()) {
+        while (!StdIn.isEmpty()) {
             String theString = StdIn.readString();
-            if (StdRandom.bernoulli(1/i)){
+            if (StdRandom.bernoulli(1 / i)) {
                 winner = theString;
             }
             i++;
         }
         StdOut.println(winner);
     }
-
     }
 

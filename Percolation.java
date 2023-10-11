342b9d20e3014cf451b561fa16e52c168183832c
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-10-11 16:49:13 +0300

    Add logic for opening sites

--- Percolation.java
+++ Percolation.java
@@ -6,9 +6,16 @@ public class Percolation {
     private int dimension;
     WeightedQuickUnionUF wqu;
 
+    int[] opened;
+
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int dimension) {
         this.dimension = dimension;
+
+        opened = new int[dimension * dimension];
+        for ( int i = 0; i < opened.length; i++ ) {
+            opened[i] = 0; //initially all sites are closed hence 0, when opened turn to 1
+        }
 
         wqu = new WeightedQuickUnionUF(dimension * dimension + 2); //the +2 is for the above and below virtual sites
 
@@ -26,16 +33,39 @@ public class Percolation {
 
     }
 
+    //my method
     public int connected(int p) {
         return wqu.find(p);
     }
     
     // opens the site (row, col) if it is not open already
     public void open(int row, int col){
+
+        int currentSite = row * col + col;
+
+        int siteToLeft = row * (col-1) + (col-1);
+        int siteToRight = row * (col + 1) + (col + 1);
         
-        if (!isOpen(row-1, col-1)) {
-            
-        }
+        int siteUp = (row - 1) * col + (col);
+        int siteDown = (row + 1) * col + (col);
+
+        opened[currentSite] = 1;
+        
+      if (wqu.find(currentSite) != wqu.find(siteToLeft) && opened[siteToLeft] == 1) {
+        wqu.union(currentSite, siteToLeft);
+      }
+
+      if (wqu.find(currentSite) != wqu.find(siteToRight) && opened[siteToRight] == 1) {
+        wqu.union(currentSite, siteToRight);
+      }
+
+      if (wqu.find(currentSite) != wqu.find(siteUp) && opened[siteUp] == 1) {
+        wqu.union(currentSite, siteUp);
+      }
+
+      if (wqu.find(currentSite) != wqu.find(siteDown) && opened[siteDown] == 1) {
+        wqu.union(currentSite, siteDown);
+      }
     }
 
     // is the site (row, col) open?
@@ -54,12 +84,17 @@ public class Percolation {
 
     // test client (optional)
     public static void main(String[] args){
-        int n = 3;
+        int n = 4;
         Percolation perc = new Percolation(n);
         for ( int i = 0; i < n*n + 2; i++) {
             System.out.println(perc.connected(i));
         }
-        
+
+        perc.open(0,1);
+        perc.open(1,0);
+        perc.open(2,0);
+
+        perc.connected(6);
         
     
     }

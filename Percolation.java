c4411f4828a47c9bae8541aad9effa0bf4d1a247
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-10-13 11:48:24 +0300

    Open sites and check percolation

--- Percolation.java
+++ Percolation.java
@@ -5,6 +5,8 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
     private int dimension;
     WeightedQuickUnionUF wqu;
+    int topVirtualSite;
+    int bottomVirtualSite;
 
     int[] opened;
 
@@ -17,18 +19,18 @@ public class Percolation {
             opened[i] = 0; //initially all sites are closed hence 0, when opened turn to 1
         }
 
-        wqu = new WeightedQuickUnionUF(dimension * dimension + 2); //the +2 is for the above and below virtual sites
+        wqu = new WeightedQuickUnionUF(dimension * dimension + 2); //the +2 is for the top and bottom virtual sites
 
-        int firstVirtualSite = dimension* dimension;
-        int secondVirtualSite = dimension * dimension + 1;
+        topVirtualSite = dimension* dimension; //second from last element = top virtual site
+        bottomVirtualSite = dimension * dimension + 1; //last element = bottom virtual site
 
         for (int i = 0; i < dimension; i++) {
-            wqu.union(firstVirtualSite, i); //connect all the above row sites to the first virtual site
+            wqu.union(topVirtualSite, i); //connect all the top row sites to the top virtual site
         }
 
         int lastElement = dimension * dimension - 1;
         for (int i = lastElement; i > (lastElement - 3); i-- ) {
-            wqu.union(secondVirtualSite, i); //connect all the bottom row sites to the second virtual site
+            wqu.union(bottomVirtualSite, i); //connect all the bottom row sites to the second virtual site
         }
 
     }
@@ -41,36 +43,45 @@ public class Percolation {
     // opens the site (row, col) if it is not open already
     public void open(int row, int col){
 
-        int currentSite = row * col + col;
+        // int currentSite = row * col + col;
+        int currentSite = (row * dimension) + col;
 
-        int siteToLeft = row * (col-1) + (col-1);
-        int siteToRight = row * (col + 1) + (col + 1);
+        int siteToLeft = (row * dimension) + (col-1);
+        int siteToRight = row * (dimension) + (col + 1);
         
-        int siteUp = (row - 1) * col + (col);
-        int siteDown = (row + 1) * col + (col);
+        int siteUp = (row - 1) * dimension + (col);
+        int siteDown = (row + 1) * dimension + (col);
 
         opened[currentSite] = 1;
-        
-      if (wqu.find(currentSite) != wqu.find(siteToLeft) && opened[siteToLeft] == 1) {
-        wqu.union(currentSite, siteToLeft);
+
+      if (col != 0){
+        if (wqu.find(currentSite) != wqu.find(siteToLeft) && opened[siteToLeft] == 1) {
+          wqu.union(currentSite, siteToLeft);
+        }
+      }
+      
+      if (col != dimension-1) {
+        if (wqu.find(currentSite) != wqu.find(siteToRight) && opened[siteToRight] == 1) {
+          wqu.union(currentSite, siteToRight);
+        }
       }
 
-      if (wqu.find(currentSite) != wqu.find(siteToRight) && opened[siteToRight] == 1) {
-        wqu.union(currentSite, siteToRight);
+      if (row != 0) {
+        if (wqu.find(currentSite) != wqu.find(siteUp) && opened[siteUp] == 1) {
+          wqu.union(currentSite, siteUp);
+        }
       }
-
-      if (wqu.find(currentSite) != wqu.find(siteUp) && opened[siteUp] == 1) {
-        wqu.union(currentSite, siteUp);
-      }
-
-      if (wqu.find(currentSite) != wqu.find(siteDown) && opened[siteDown] == 1) {
-        wqu.union(currentSite, siteDown);
+      
+      if (row != dimension-1) {
+        if (wqu.find(currentSite) != wqu.find(siteDown) && opened[siteDown] == 1) {
+          wqu.union(currentSite, siteDown);
+        }
       }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return true;
+        return opened[(row * col) + col] == 1;
     }
 
     // is the site (row, col) full?
@@ -80,21 +91,31 @@ public class Percolation {
     public int numberOfOpenSites(){ return 1;}
 
     // does the system percolate?
-    public boolean percolates(){ return true;}
+    public boolean percolates(){ 
+      return connected(topVirtualSite) == connected(bottomVirtualSite);
+    }
 
     // test client (optional)
     public static void main(String[] args){
-        int n = 4;
+        int n = 3;
         Percolation perc = new Percolation(n);
+
         for ( int i = 0; i < n*n + 2; i++) {
             System.out.println(perc.connected(i));
         }
 
-        perc.open(0,1);
+        perc.open(0,0);
         perc.open(1,0);
         perc.open(2,0);
 
-        perc.connected(6);
+        for ( int i = 0; i < n*n + 2; i++) {
+            System.out.println(perc.connected(i));
+        }
+        System.out.println("_______________________________");
+        for (int i = 0; i < perc.opened.length; i++) {
+          System.out.println(perc.opened[i]);
+        }
+        System.out.println(perc.percolates());
         
     
     }
